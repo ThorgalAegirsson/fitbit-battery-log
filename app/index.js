45cee941187f5bb00e79a6f1f84e21a4f651800e
Radek Szumski <usa.rs@live.com>
2019-09-30 00:18:07 +0200

    Redo UI

--- app/index.js
+++ app/index.js
@@ -2,12 +2,21 @@ import { battery, charger } from 'power';
 import document from 'document';
 import * as fs from 'fs';
 
+
 // DOM refs
 const batteryValue = document.getElementById('batteryValue');
 const batteryCharge = document.getElementById('batteryCharge');
 const connectState = document.getElementById('connectState');
 const lastChargedDateField = document.getElementById('lastChargedDate');
 const lastChargedTimeField = document.getElementById('lastChargedTime');
+const container = document.getElementById("container");
+
+// Get the selected index
+let currentIndex = container.value;
+
+// Set the selected index
+container.value = 0; // jump to first slide
+
 let connectDate = null;
 
 try {
@@ -19,7 +28,7 @@ try {
 // init GUI
 batteryValue.text = `${battery.chargeLevel}%`;
 batteryCharge.width = 141*battery.chargeLevel/100;
-connectState.text = charger.connected ? 'Charger connected' : 'Charger disconnected';
+connectState.text = charger.connected ? 'Plugged in' : 'Unplugged';
 if (connectDate) {
     lastChargedDateField.text = connectDate.toDateString();
     lastChargedTimeField.text = `${connectDate.getHours()}:${connectDate.getMinutes()}`;
@@ -27,6 +36,7 @@ if (connectDate) {
 
 console.log('=== CHARGER ===');
 console.log(`charger connected: ${charger.connected}`);
+console.log(`charger power is good: ${charger.powerIsGood}`);
 
 battery.onchange = evt => {
     batteryValue.text = `${battery.chargeLevel}%`;
@@ -36,24 +46,19 @@ battery.onchange = evt => {
 
 charger.onchange = evt => {
     console.log(`CHARGER charger connected: ${charger.connected}`);
-    connectState.text = charger.connected ? 'Charger connected' : 'Charger disconnected';
+    connectState.text = charger.connected ? charger.powerIsGood ? 'Plugged in, charging' : 'Plugged in, not charging' : 'Unplugged';
     if (!charger.connected) {
         connectDate = new Date();
         // save the date to a file on the device, later utilize companion and/or cloud
         fs.writeFileSync('lastCharged.txt', connectDate, 'cbor');
-        lastChargedDateField.text = connectDate.toDateString();
-        lastChargedTimeField.text = `${connectDate.getHours()}:${connectDate.getMinutes()}`;
+        lastChargedDateField.text = connectDate.toLocaleDateString();
+        // lastChargedTimeField.text = `${connectDate.getHours()}:${connectDate.getMinutes()}`;
+        lastChargedTimeField.text = connectDate.toLocaleTimeString();
         console.log(connectDate);
         console.log(connectDate.toLocaleDateString());
         console.log(connectDate.toLocaleTimeString());
         console.log(connectDate.toDateString());
         console.log(connectDate.toTimeString());
         console.log(connectDate.getHours());
-        console.log(connectDate.getUTCDate());
-        console.log(connectDate.getUTCHours());
     }
 };
-
-console.log(`charger, power is good: ${charger.powerIsGood}`);
-
-
